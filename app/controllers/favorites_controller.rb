5cc8220d97be5438e67fcf6dd4c538b1a65da37d
Billy1339 <william.m.sargent@gmail.com>
2014-10-20 15:30:46 -0400

    favorites views working

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,12 +4,20 @@ class FavoritesController < ApplicationController
     @user = current_user
     @reminder = Reminder.all
     @reminder.each do |t|
-      if @reminder.user_id == @user.id && @reminder.favorite == true
-
+      if t.user_id == @user.id && t.favorite == true
+        @favorite = Favorite.create(fav_params)
+        @favorite[:picture] = t.picture
+        @favorite[:phone_number] = t.phone_number
       end
     end
   end
 
-  private
+  def new
+    @favorite = Favorite.new
+  end
 
+  private
+  def fav_params
+    params.fetch(:favorite, {}).permit(:picture, :phone_number)
+  end
 end
