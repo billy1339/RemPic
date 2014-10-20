fd1f4d45e999fe110d123eb6fd275c5a6144e2de
Billy1339 <william.m.sargent@gmail.com>
2014-10-19 20:32:41 -0400

    made a form for reminders, and created new and create methods for reminders

--- app/controllers/reminders_controller.rb
+++ app/controllers/reminders_controller.rb
@@ -1,7 +1,29 @@
 class RemindersController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
+
 
   def index
     @user = current_user
   end
+
+  def new
+    @user = current_user
+    @reminder = Reminder.new
+  end
+
+  def create
+    @reminder = Reminder.create(reminder_params)
+    @reminder.user = current_user
+    @reminder.save
+    redirect_to reminders_path
+  end
+
+
+  private
+
+  def reminder_params
+    params.require(:reminder).permit(:text, :phone_number, :time, :picture)
+  end
+
 
 end
